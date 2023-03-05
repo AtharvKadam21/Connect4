e59005ddb785432b87b3ae3eff0fe47f0e0c15a1
atharvkadam21 <atharvkadam82@gmail.com>
2023-03-06 03:36:27 +0530

    can insert values to database

--- Menu.py
+++ Menu.py
@@ -25,7 +25,7 @@ dpg.create_context()
 dpg.create_viewport(title='Game', width=700, height=600)
 dpg.setup_dearpygui()
 
-def ModeValue(app_data):
+def ModeValue(sender, app_data):
     if app_data[1]=='1Play':
         oneplay()
     elif app_data[1]=='2Play':
@@ -33,27 +33,33 @@ def ModeValue(app_data):
     else:
         threeplay()
 
-def dbEntry(app_data, user_data):
-    print(app_data)
-    UserName=dpg.get_value(InName)
+def getdbValues(UserName, BCol, TCol):
+    b1=(UserName, BCol, TCol)
+    s="INSERT INTO user_accounts VALUES(%s,%s,%s)"
+    cur.execute(s,b1)
+    mydb.commit()
+
+def dbEntry(sender, app_data, user_data):
     BCol='Blue'
     TCol='Red'
-
+    UserName=dpg.get_value(InName)
     if app_data[1]=='BColBl':
         BCol='Blue'
+        print(BCol)        
+        
     if app_data[1]=='BColPurp':
         BCol='Purple'
+        print(BCol)
+
     if app_data[1]=='TColRd':
         TCol='Red'
+        print(TCol)
+
     if app_data[1]=='TColPurp':
         TCol='Purple'
+        print(TCol)
 
-    if app_data[1]=='Tcol':
-        dpg.get_value(tag='Tcol')
-
-    if app_data[1]=='SaveChanges':
-        print(UserName, BCol, TCol)
-
+    getdbValues(UserName, BCol, TCol)
 
 #=========item_registry(s)==========
 with dpg.item_handler_registry(tag="widget handler") as handler:
@@ -62,36 +68,39 @@ with dpg.item_handler_registry(tag="widget handler") as handler:
 with dpg.item_handler_registry(tag="dbvalues") as handler:
     dpg.add_item_clicked_handler(callback=dbEntry)
 
-with dpg.window(label='Select Mode', modal=True, show=False, tag='b0', no_title_bar=True):
-        dpg.add_button(label="VS AI", tag='1Play')
-        dpg.add_button(label="2 Players", tag='2Play')
-        dpg.add_button(label="3 Players", tag='3Play')
-        dpg.add_button(label="Back", width=75, callback=lambda: dpg.configure_item("b0", show=False))
+with dpg.window(label='Select Mode', modal=True, show=False, tag='b0', no_title_bar=True,pos=(280,50)):
+        dpg.add_button(label="VS AI", tag='1Play',width=100, height=50)
+        dpg.add_button(label="2 Players", tag='2Play',width=100, height=50)
+        dpg.add_button(label="3 Players", tag='3Play',width=100, height=50)
+        dpg.add_button(label="Back", callback=lambda: dpg.configure_item("b0", show=False),width=100, height=50)
 
-with dpg.window(label='Account', modal=True, show=False, tag='b1', no_title_bar=True):
-    InName=dpg.add_input_text(default_value="Player", tag="Pname")
+with dpg.window(label='Account', modal=True, show=False, tag='b1', no_title_bar=True,pos=(230, 50)):
+    with dpg.group(horizontal=True):
+        dpg.add_text("Name")
+        InName=dpg.add_input_text(default_value="Player", tag="Pname")
 
     with dpg.group(horizontal=True):
         dpg.add_text("Board Theme")
-        dpg.add_button(label="Blue", tag="BColBl")
-        dpg.add_button(label="Purple", tag="BColPurp")
-       
+        dpg.add_button(label="Blue", tag="BColBl", width=100, height=30)
+        dpg.add_button(label="Purple", tag="BColPurp", width=100, height=30)
+
     with dpg.group(horizontal=True):
         dpg.add_text("Token color")
-        dpg.add_button(label="Red", tag="TColRd")
-        dpg.add_button(label="Purple", tag="TColPurp")
+        dpg.add_button(label="Red", tag="TColRd",width=100, height=30)
+        dpg.add_button(label="Purple", tag="TColPurp",width=100, height=30)
     
-    dpg.add_button(label="Save", tag="SaveChanges")
+    with dpg.group(horizontal=True):
+        dpg.add_button(label="Save", tag="SaveChanges",width=100, height=30)
+        dpg.add_button(label="Back", callback=lambda: dpg.configure_item("b1", show=False),width=100, height=30)
 
-    dpg.add_button(label="Back", width=75, callback=lambda: dpg.configure_item("b1", show=False))
 
-
-with dpg.window(tag="Primary Window",width=800, height=600):
-    dpg.add_text("Connect 4")
-    dpg.add_button(label="Play", callback=lambda: dpg.configure_item('b0', show=True))
-    dpg.add_button(label="Account", callback=lambda: dpg.configure_item('b1', show=True))
-    dpg.add_button(label="Leaderboard")
-    dpg.add_button(label="Quit", callback=lambda: dpg.configure_item('Primary Window', show=False))
+with dpg.window(tag="Primary Window", width=700, height=600):
+    with dpg.group(pos=(300,50)):
+        dpg.add_text("Connect 4")
+        dpg.add_button(label="Play", callback=lambda: dpg.configure_item('b0', show=True),width=100, height=40)
+        dpg.add_button(label="Account", callback=lambda: dpg.configure_item('b1', show=True),width=100, height=40)
+        dpg.add_button(label="Leaderboard",width=100, height=40)
+        dpg.add_button(label="Quit", callback=lambda: dpg.configure_item('Primary Window', show=False),width=100, height=40)
 
 #========binding item to registry=========
 dpg.bind_item_handler_registry('1Play', "widget handler")
--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 import dearpygui.dearpygui as dpg
 
 dpg.create_context()
-
-def change_text(sender, app_data):
-    dpg.set_value("text item", f"Mouse Button ID: {app_data}")
-
-with dpg.window(width=500, height=300):
-    dpg.add_text("Click me with any mouse button", tag="text item")
-    with dpg.item_handler_registry(tag="widget handler") as handler:
-        dpg.add_item_clicked_handler(callback=change_text)
-    dpg.bind_item_handler_registry("text item", "widget handler")
-
-dpg.create_viewport(title='Custom Title', width=800, height=600)
+dpg.create_viewport(title='Game', width=700, height=600)
 dpg.setup_dearpygui()
+
+def on_button_press(sender, app_data, user_data):
+    selected_color = app_data
+    print(f"Selected color: {selected_color}")
+    name_value = dpg.get_value("Name Input")
+    print(f"Name value: {name_value}")
+
+with dpg.window():
+    dpg.add_text("Enter your name:")
+    dpg.add_input_text(default_value="Name Input")
+
+    dpg.add_text("Select your favorite color:")
+    with dpg.group():
+        dpg.add_button(label="Red", callback=on_button_press, callback_data="red")
+        dpg.add_button("Green", callback=on_button_press, callback_data="green")
+        dpg.add_button("Blue", callback=on_button_press, callback_data="blue")
+
 dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
